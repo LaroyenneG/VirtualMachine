0637f01ac2529cc7300b24d58250a5d55c92dd6c
laroyenneg <laroyenneg@gmail.com>
2019-05-04 14:52:22 +0200

    C version

--- src/processor/exception/InvalidCodeTypeException.java
+++
@@ -1,7 +0,0 @@
-package processor.exception;
-
-public class InvalidCodeTypeException extends Exception {
-    public InvalidCodeTypeException(int code) {
-        super("Invalid code : " + code);
-    }
-}
